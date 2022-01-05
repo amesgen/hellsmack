4ef5218622b85c79e9aea4f849b55f8155105733
amesgen <amesgen@amesgen.de>
2022-01-05 01:21:10 +0100

    Use pre-commit-hooks.nix, use nur-packages

--- flake.nix
+++ flake.nix
@@ -1,14 +1,32 @@
 {
   inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
   inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+  inputs.nur.url = "github:nix-community/NUR";
+  # TODO remove
+  inputs.nur-amesgen = {
+    url = "github:amesgen/nur-packages";
+    inputs.nixpkgs.follows = "nixpkgs";
+    inputs.flake-utils.follows = "flake-utils";
+  };
+  inputs.pre-commit-hooks = {
+    url = "github:cachix/pre-commit-hooks.nix";
+    inputs.nixpkgs.follows = "nixpkgs";
+    inputs.flake-utils.follows = "flake-utils";
+  };
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  outputs = { self, nixpkgs, flake-utils, haskellNix }:
+  outputs = { self, nixpkgs, flake-utils, haskellNix, nur, nur-amesgen, pre-commit-hooks }:
     flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
       let
         pkgs = import nixpkgs {
           inherit system;
           inherit (haskellNix) config;
-          overlays = [ haskellNix.overlay ];
+          overlays = [
+            haskellNix.overlay
+            (_: pkgs: {
+              nur = import nur { nurpkgs = pkgs; inherit pkgs; }
+                // { amesgen = nur-amesgen.packages.${system}; };
+            })
+          ];
         };
         inherit (pkgs) lib;
         hsPkgs = pkgs.haskell-nix.cabalProject {
@@ -44,17 +62,30 @@
               --hie-directory ${hellsmack.components.library.hie} \
               --hie-directory ${hellsmack.components.tests.tasty.hie}
           '';
-          hlint = pkgs.runCommand "hellsmack-hlint"
-            { buildInputs = [ (hsPkgs.tool "hlint" "latest") ]; } ''
-            mkdir -p $out
-            cd ${./.}
-            hlint src app test
-          '';
+          pre-commit-check =
+            let ormolu = pkgs.nur.amesgen.ormolu; in
+            pre-commit-hooks.lib.${system}.run {
+              src = ./.;
+              hooks = {
+                nixpkgs-fmt.enable = true;
+                ormolu = {
+                  enable = true;
+                  entry = lib.mkForce "${ormolu}/bin/ormolu -e --mode inplace";
+                };
+                hlint.enable = true;
+              };
+              tools = {
+                inherit ormolu;
+                hlint = hsPkgs.tool "hlint" "latest";
+              };
+            };
         };
         devShell = hsPkgs.shellFor {
           tools = { cabal = { }; };
+          buildInputs = [ pkgs.nur.amesgen.cabal-docspec ];
           withHoogle = false;
           exactDeps = true;
+          inherit (self.checks.${system}.pre-commit-check) shellHook;
         };
 
         packages = {
